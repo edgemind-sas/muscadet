33d3b3a490e65b2fb472e17149055d680d5e39d8
Roland Donat <roland.donat@edgemind.net>
2024-01-22 16:01:28 +0100

    Update cod3s first example

--- examples/cod3s_first_example/system.py
+++ examples/cod3s_first_example/system.py
@@ -25,18 +25,25 @@ class MySystem(muscadet.System):
 
         param_s = dict(
             name="frun",
+            failure_rate=1/5,
+            repair_rate=1/3,
+            failure_effects=[(".*_available_out", False)],
+        )
+
+        param_c1 = dict(
+            name="frun",
+            failure_time=7,
+            repair_time=2,
+            failure_effects=[(".*_available_out", False)],
+        )
+
+        param_c2 = dict(
+            name="frun",
             failure_time=5,
             repair_time=3,
             failure_effects=[(".*_available_out", False)],
         )
 
-        param_c = dict(
-            name="frun",
-            failure_time=12,
-            repair_time=2,
-            failure_effects=[(".*_available_out", False)],
-        )
-        
-        self.comp["S"].add_delay_failure_mode(**param_s)
-        self.comp["C1"].add_delay_failure_mode(**param_c)
-        self.comp["C2"].add_delay_failure_mode(**param_c)
+        self.comp["S"].add_exp_failure_mode(**param_s)
+        self.comp["C1"].add_delay_failure_mode(**param_c1)
+        self.comp["C2"].add_delay_failure_mode(**param_c2)
