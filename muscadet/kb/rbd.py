442fcc1dfa856efb014becc7f519fcf4b71444c7
loic <loic.perez@edgemind.net>
2024-06-20 09:16:01 +0200

    rbd class centralized in kb folder in muscadet project

---
+++ muscadet/kb/rbd.py
@@ -0,0 +1,56 @@
+import muscadet
+
+# Global attributes
+# ==================
+flow1 = "is_ok"
+
+
+# Components classes
+# ==================
+class Source(muscadet.ObjFlow):
+    def add_flows(self, **kwargs):
+        super().add_flows(**kwargs)
+
+        self.add_flow_out(
+            name=flow1,
+            var_prod_default=True,
+        )
+
+
+class SourceTrigger(muscadet.ObjFlow):
+    def add_flows(self, **kwargs):
+        super().add_flows(**kwargs)
+
+        self.add_flow_out_on_trigger(
+            name=flow1,
+            trigger_time_up=0,
+            trigger_time_down=0,
+            trigger_logic="and",
+            var_prod_default=True,
+        )
+
+
+class Block(muscadet.ObjFlow):
+    def add_flows(self, **kwargs):
+        super().add_flows(**kwargs)
+
+        self.add_flow_in(
+            name=flow1,
+        )
+
+        self.add_flow_out(
+            name=flow1,
+            var_prod_cond=[
+                flow1,
+            ],
+        )
+
+
+class Target(muscadet.ObjFlow):
+    def add_flows(self, **kwargs):
+        super().add_flows(**kwargs)
+
+        self.add_flow_in(
+            name=flow1,
+            logic="and",
+        )
